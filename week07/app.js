f1ac65793fa978b3a2189e3425fca3a378c97435
borybop <168351251+borybop@users.noreply.github.com>
2024-11-19 18:13:03 +0900

    리액트2팀 7주차 스터디

---
+++ week07/app.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { useState } from "react";
+import Controller from "./component/Controller";
+import Viewer from "./component/Viewer";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} /> ①
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount} /> ②
+      </section>
+    </div>
+  );
+}
+export default App;
